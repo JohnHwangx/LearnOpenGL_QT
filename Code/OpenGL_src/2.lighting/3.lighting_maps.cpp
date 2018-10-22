aa502767cd859801c7acbff33e574e85d3e29216
johnhuang <johnhuangx@outlook.com>
2018-10-22 19:27:51 +0800

    sdasda

--- Code/OpenGL_src/2.lighting/3.lighting_maps.cpp
+++ Code/OpenGL_src/2.lighting/3.lighting_maps.cpp
@@ -1,5 +1,75 @@
 #include "3.lighting_maps.h"
 
-void LIGHTING_MAPS::lighting_maps::show(std::string & message)
+#include "Vertices.h"
+
+using namespace std;
+using namespace glm;
+using namespace LIGHTING_MAPS;
+
+void lighting_maps::show(std::string & message)
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Learn OpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		message.append("Failed to create window.");
+		glfwTerminate();
+		return;
+	}
+
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		message.append("Failed to load GLAD.");
+		return;
+	}
+
+	glEnable(GL_DEPTH_TEST);
+
+	Shader shader("", "");
+
+	unsigned int cubeVAO, VBO;
+	glGenVertexArrays(1, &cubeVAO);
+	glBindVertexArray(cubeVAO);
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(VERTICES::vertices_color), VERTICES::vertices_color, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(sizeof(float) * 3));
+	glEnableVertexAttribArray(1);
+
+	unsigned int lightVAO;
+	glGenVertexArrays(1, &lightVAO);
+	glBindVertexArray(lightVAO);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+
+}
+
+void LIGHTING_MAPS::lighting_maps::processInput(GLFWwindow * window)
+{
+}
+
+void LIGHTING_MAPS::lighting_maps::framebuffer_callback(GLFWwindow * window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void LIGHTING_MAPS::lighting_maps::mouse_callback(GLFWwindow * window, double xPos, double yPos)
+{
+}
+
+void LIGHTING_MAPS::lighting_maps::scroll_callback(GLFWwindow * window, double xoffset, double yoffset)
 {
 }
--- Code/OpenGL_src/2.lighting/3.lighting_maps.h
+++ Code/OpenGL_src/2.lighting/3.lighting_maps.h
@@ -1,8 +1,26 @@
 #pragma once
+
+#include "Shader.h"
+#include "Camera.h"
+
+#include <GLFW/glfw3.h>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include <iostream>
 namespace LIGHTING_MAPS {
 	class lighting_maps {
 	public:
 		void show(std::string& message);
+	private:
+		unsigned int SCR_WIDTH;
+		unsigned int SCR_HEIGHT;
+
+		static CAMERA::Camera camera;
+
+		void processInput(GLFWwindow* window);
+		static void framebuffer_callback(GLFWwindow* window, int width, int height);
+		static void mouse_callback(GLFWwindow* window, double xPos, double yPos);
+		static void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 	};
 }
