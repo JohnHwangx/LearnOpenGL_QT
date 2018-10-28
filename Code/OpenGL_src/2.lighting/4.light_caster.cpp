98d398093741a2c1e18524fb48a07c739df629f8
huangqiang <johnhuangx@outlook.com>
2018-10-28 23:59:07 +0800

    ghhjgjh

--- Code/OpenGL_src/2.lighting/4.light_caster.cpp
+++ Code/OpenGL_src/2.lighting/4.light_caster.cpp
@@ -2,4 +2,145 @@
 
 void LIGHT_CASTER::light_caster::show(std::string & message)
 {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Learn OpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		message.append("Failed to create window");
+		glfwTerminate();
+		return;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framesize_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		message.append("Failed to load GLAD");
+		return;
+	}
+
+	glEnable(GL_DEPTH_TEST);
+
+	Shader cubeShader("", "");
+	Shader lightShader("", "");
+
+	unsigned int cubeVAO, VBO;
+	glGenVertexArrays(1, &cubeVAO);
+	glBindVertexArray(cubeVAO);
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(VERTICES::vertices_normal_texture), VERTICES::vertices_normal_texture, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+
+	unsigned int diffuseTex = loadTexture("", message);
+	unsigned int specularTex = loadTexture("", message);
+
+	cubeShader.use();
+	cubeShader.setInt("", 0);
+	cubeShader.setInt("", 1);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		float currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+		glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+
+		cubeShader.use();
+
+	}
+}
+
+unsigned int LIGHT_CASTER::light_caster::loadTexture(const char * path, std::string& message)
+{
+	unsigned int textureID;
+
+	glGenTextures(1, &textureID);
+
+	int width, height, nrChannel;
+	unsigned char* data = stbi_load(path, &width, &height, &nrChannel, 0);
+	if (data)
+	{
+		GLenum format;
+		if (nrChannel == 1)
+			format = GL_RED;
+		else if (nrChannel == 3)
+			format = GL_RGB;
+		else  if (nrChannel == 4)
+			format = GL_RGBA;
+
+		glBindTexture(GL_TEXTURE_2D, textureID);
+		glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+	}
+	else
+	{
+		message.append("Failed to generate Texture");
+	}
+	stbi_image_free(data);
+
+	return textureID;
+}
+
+void LIGHT_CASTER::light_caster::processInput(GLFWwindow * window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwWindowShouldClose(window);
+
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.ProcessKeyboard(CAMERA::FORWARD, deltaTime);
+	else if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.ProcessKeyboard(CAMERA::BACKWARD, deltaTime);
+	else if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera.ProcessKeyboard(CAMERA::LEFT, deltaTime);
+	else if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.ProcessKeyboard(CAMERA::RIGHT, deltaTime);
+}
+
+void LIGHT_CASTER::light_caster::framesize_callback(GLFWwindow * window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void LIGHT_CASTER::light_caster::mouse_callback(GLFWwindow * window, double xPos, double yPos)
+{
+	if (firstMouse)
+	{
+		lastX = xPos;
+		lastY = yPos;
+		firstMouse = false;
+	}
+
+	float xoffset = xPos - lastX;
+	float yoffset = lastY - yPos;
+
+	lastX = xPos;
+	lastY = yPos;
+
+	camera.ProcessMouseMove(xoffset, yoffset);
+}
+
+void LIGHT_CASTER::light_caster::scroll_callback(GLFWwindow * window, double xoffset, double yoffset)
+{
+	camera.ProcessMouseScroll(yoffset);
 }
--- Code/OpenGL_src/2.lighting/4.light_caster.h
+++ Code/OpenGL_src/2.lighting/4.light_caster.h
@@ -1,8 +1,33 @@
 #pragma once
+#include "Shader.h"
+#include "Camera.h"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <Vertices.h>
+
+#include "stb_image.h"
+
 #include <iostream>
 namespace LIGHT_CASTER {
 	class light_caster {
 	public:
 		void show(std::string& message);
+	private:
+		unsigned int SCR_WIDTH;
+		unsigned int SCR_HEIGHT;
+
+		float deltaTime;
+		float lastFrame;
+
+		static bool firstMouse;
+		static float lastX;
+		static float lastY;
+		static CAMERA::Camera camera;
+
+		unsigned int loadTexture(const char* path, std::string& message);
+		void processInput(GLFWwindow* window);
+		static void framesize_callback(GLFWwindow* window, int width, int height);
+		static void mouse_callback(GLFWwindow* window, double xPos, double yPos);
+		static void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 	};
 }
